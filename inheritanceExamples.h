383b2fe05ae9cf0e5e4822f21a9a20b6a33a1b73
Sachin Gupta <hapysachin91@gmail.com>
2025-05-12 14:42:15 -0700

    Create inheritanceExamples.h

---
+++ inheritanceExamples.h
@@ -0,0 +1,10 @@
+#ifndef INHERITANCE_EXAMPLES_H
+#define INHERITANCE_EXAMPLES_H
+
+#include <iostream>
+#include <string>
+
+// Function declarations
+void inherianceExamples();
+
+#endif // INHERITANCE_EXAMPLES_H
